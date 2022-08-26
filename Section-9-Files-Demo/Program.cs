674b8d8fa65a4ba911e366dcbd765c87f6eead0a
Faysal Regal <faysalregal@gmail.com>
2022-08-26 19:58:44 +0100

    worked with files class

--- Section-9-Files-Demo/Program.cs
+++ Section-9-Files-Demo/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.IO;
 
-namespace Section9FilesDemo
+namespace CsharpFundamentals
 {
-    class MainClass
+    class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!");
+
+            var path = @"/Users/faysalregal/Desktop/C# Fundamentals/Section 9 Working with Files/Demo/Path2/il_1588xN.2017618038_lcju.webp";
+
+            // File.Copy(@"/Users/faysalregal/Desktop/C# Fundamentals/Section 9 Working with Files/Demo/Path1/il_1588xN.2017618038_lcju.webp", @"/Users/faysalregal/Desktop/C# Fundamentals/Section 9 Working with Files/Demo/Path2/il_1588xN.2017618038_lcju.webp", true);
+            //File.Delete(path);
+            if (File.Exists(path))
+            {
+                Console.WriteLine("Great Stuff!");
+            }
+
+
         }
     }
 }
